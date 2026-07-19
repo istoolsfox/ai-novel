2ae160bec4522a71b488d813ff7dae3dbbaa38cd
wlxb625 <133953421+wlxb625@users.noreply.github.com>
2026-07-19 23:29:00 +0800

    Bootstrap runtime reliability

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,6 +1,7 @@
 from .impact_planning_bootstrap import bootstrap_impact_planning
 from .memory_bootstrap import bootstrap_layered_memory
 from .obsidian_bootstrap import bootstrap_obsidian_export
+from .runtime_bootstrap import bootstrap_runtime_reliability
 from .story_graph_bootstrap import bootstrap_story_graph
 from .worldline_bootstrap import bootstrap_worldlines
 
@@ -9,3 +10,4 @@ bootstrap_story_graph()
 bootstrap_impact_planning()
 bootstrap_worldlines()
 bootstrap_obsidian_export()
+bootstrap_runtime_reliability()
